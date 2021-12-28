ef0067c8eb61602d87f4a201952c2eaf841fbb9b
suraj9060 <surajpandit831996@gmail.com>
2021-12-28 10:12:27 +0530

    component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React from "react"
 import './App.css';
 
+
 function App() {
+  let [counter , setCounter ] = React.useState(0);
+
+  const add = (value) => {
+   setCounter(counter + value); 
+    
+  }
+  const double = (value) => {
+      setCounter(counter * value)
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Counter app</h1>
+      <h2>{counter}</h2>
+
+      <button onClick={() => add(1)}>add</button>
+      <button onClick={() => add(-1)}>reduce</button>
+      <button onClick={() => double(2)}>double</button>
     </div>
   );
 }
+
+
+
 
 export default App;
